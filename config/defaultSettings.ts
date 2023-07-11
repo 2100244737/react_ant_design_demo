def62eba3c0203446c87e223f29a0dd8c7a918df
songwenbo <2100244737@qq.com>
2023-07-11 17:30:19 +0800

    2023-07-11

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -3,19 +3,18 @@ import { Settings as LayoutSettings } from '@ant-design/pro-components';
 const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
-} = {
-  navTheme: 'light',
-  // 拂晓蓝
-  primaryColor: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'Ant Design Pro',
-  pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
+} ={
+  "navTheme": "light",
+  "primaryColor": "#1890ff",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": true,
+  "fixSiderbar": true,
+  "pwa": false,
+  "logo": "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  "headerHeight": 48,
+  "splitMenus": false,
+  "footerRender": false
 };
 
 export default Settings;
